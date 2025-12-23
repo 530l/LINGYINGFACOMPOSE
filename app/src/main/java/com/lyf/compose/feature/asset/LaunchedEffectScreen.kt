d6b67725ca4af4024ff290d73ac4c4000dd6b2dd
linyingfa <linyingfa@atmob.com>
2025-12-23 18:32:30 +0800

    //补充Effect

---
+++ app/src/main/java/com/lyf/compose/feature/asset/LaunchedEffectScreen.kt
@@ -0,0 +1,70 @@
+package com.lyf.compose.feature.asset
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import com.lyf.compose.core.nav3.LocalNavigator
+import com.lyf.compose.core.ui.components.scaffold.AppScaffold
+import kotlinx.coroutines.delay
+
+
+suspend fun fetchDataFromNetwork(): String {
+    // 模拟延迟2秒，例如网络请求或数据库查询
+    delay(2000)
+    return "Fetched data from server"
+}
+
+@Composable
+fun LaunchedEffectScreen() {
+    val navigator = LocalNavigator.current
+    var data by remember { mutableStateOf("Loading...") }  // 用于保存加载的数据
+    var isLoading by remember { mutableStateOf(true) } // 用于记录加载状态
+    LaunchedEffect(Unit) {// 使用 LaunchedEffect 加载数据
+        data = try {
+            // 模拟网络请求
+            fetchDataFromNetwork()
+        } catch (e: Exception) {
+            "Failed to load data: ${e.message}"
+        } finally {
+            isLoading = false
+        }
+    }
+
+    AppScaffold(onBackClick = { navigator.pop() }) {
+
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            item {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    // 展示加载状态或数据
+                    if (isLoading) {
+                        Text(text = "Loading...", style = MaterialTheme.typography.bodyMedium)
+                    } else {
+                        Text(text = data, style = MaterialTheme.typography.bodyLarge)
+                    }
+                    Text(
+                        """
+                        1.只执行一次： 当Composable组件首次进入组合（Composition）时，LaunchedEffect中的代码块会被触发执行
+                        2.响应依赖参数key变化： 如果传递的 key依赖发生变化，LaunchedEffect 会重新启动。也就是说，它的执行是依赖于key的变化
+                        3.生命周期感知： 当组件退出组合时，LaunchedEffect 中的协程会自动取消，以避免资源泄漏
+                        4.协程支持： 它运行在协程上下文中，因此特别适合处理异步任务，比如网络请求、数据库查询等
+
+            """.trimIndent(),
+                        fontSize = 16.sp,
+                    )
+
+                }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/lyf/compose/feature/asset/SideEffectScreen.kt
@@ -0,0 +1,69 @@
+package com.lyf.compose.feature.asset
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import com.lyf.compose.core.nav3.LocalNavigator
+import com.lyf.compose.core.ui.components.scaffold.AppScaffold
+import timber.log.Timber
+
+//模拟下外部状态
+val externalState = mutableListOf<String>()
+
+@Composable
+fun SideEffectScreen() {
+    val navigator = LocalNavigator.current
+    AppScaffold(onBackClick = { navigator.pop() }) {
+        var count by remember { mutableIntStateOf(0) }
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            item {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Text("Counter: $count")
+                    Button(onClick = { count++ }) {
+                        Text("Increment")
+                    }
+                    //使用 SideEffect 在每次重组后同步外部状态
+                    SideEffect {
+                        externalState.add("Count updated to $count")
+                        Timber.d("External State Synced: $externalState")
+                    }
+                    Text(
+                        """
+SideEffect会在每次重组后同步最新的 count 到外部的 externalState。
+通过这个例子，不难发现，SideEffect非常适合以下场景:
+-------------------------------------------------------------------
+1.日志记录：在每次状态更新时记录调试日志，方便排查问题。
+2.调试信息同步：将最新的 Compose 状态同步到外部工具、分析平台，或非 Compose 系统。
+3.轻量级任务：处理与 UI 状态无关的简单任务，例如统计点击次数等等。
+-------------------------------------------------------------------
+此外，我们再思考一个问题，为什么不要在SideEffect中处理大量繁重或耗时的操作？
+阻塞主线程：SideEffect始终在主线程上运行，如果在其中执行耗时的操作（如网络请求、文件读写、大量计算等），
+会阻塞 UI 更新，导致界面卡顿或掉帧，直接影响用户体验
+-------------------------------------------------------------------
+违反职责单一原则：SideEffect 的职责是执行副作用操作，而繁重任务应交由其他专用 API（如 LaunchedEffect 或后台线程）处理。
+-------------------------------------------------------------------
+滥用 SideEffect 会导致代码难以维护，甚至可能引入线程安全问题
+-------------------------------------------------------------------
+触发重组的潜在问题：如果繁重操作间接修改了Compose状态（例如改变一个 mutableState），
+可能触发额外的重组，甚至造成无限循环或性能问题
+-------------------------------------------------------------------
+            """.trimIndent(),
+                        fontSize = 16.sp,
+                    )
+
+                }
+            }
+        }
+    }
+}
+
